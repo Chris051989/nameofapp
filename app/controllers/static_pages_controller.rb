6a1ae5a1f034909f377208e0c7bc8c777e784344
Christoph <christoph.schenek@googlemail.com>
2015-07-30 15:50:18 +0200

    AdjustButtons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,13 @@ class StaticPagesController < ApplicationController
 	
 	def landing_page
 		@featured_product = Product.first
+		# Display three products from your database
 		@products = Product.limit(3)
 
+		@products = Product.all
+		# Define Counter Variable which is needed for automatically adopting the number of buttons to the number of slides on the landing page
+  		@counter = Product.count	
+    	@product = Product.new
 	end 
 
 	def index
@@ -15,6 +20,4 @@ class StaticPagesController < ApplicationController
 	def contact
 	end
 
-# Display three products from your database
- 
 end
