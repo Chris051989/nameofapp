db122887f3e9ab5491c6209f4403e75175ef18c6
Christoph <christoph.schenek@googlemail.com>
2015-07-31 15:59:32 +0200

    DynamicLinksRedirect

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to static_pages_landing_page_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -54,6 +54,7 @@ class ProductsController < ApplicationController
       end
     end
   end
+
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
