f497b832960e90693eb7db8aea03211f481768f1
Christoph <christoph.schenek@googlemail.com>
2015-08-01 12:58:46 +0200

    addedemailtemplate

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -24,11 +24,8 @@ def thank_you
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(:from => @email, 
-	  	:to => 'christoph.schenek@googlemail.com', 
-	  	:subject => "A new contact form message from #{@name}", 
-	  	:body => @message).deliver
+  UserMailer.contact_form(@email, @name, @message).deliver
+  
 end
-
 
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def contact_form(email, name, message)
+	@message = message
+  	mail(
+  		:from => email, 
+	  	:to => 'christoph.schenek@googlemail.com', 
+	  	:subject => "A new contact form message from #{name}"
+	  	)
+  end
+
+end
