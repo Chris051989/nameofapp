60ebc9dbc6a4113fa5169360e101dadfa2e1f1aa
Christoph <christoph.schenek@googlemail.com>
2015-08-02 12:26:17 +0200

    Useridentify

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,9 @@ class Ability
   include CanCan::Ability
 
       def initialize(user)
-        user ||=User.new # gues user (not logged in)
-        can :manage, User, id: user.id
-    end
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
+  end
       
 end
 
