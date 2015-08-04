2eef4189efa96839065be070946aed2c103d91d3
Christoph <christoph.schenek@googlemail.com>
2015-08-04 09:14:13 +0200

    Productcommentre

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
+# In the comments model below now 'belongs to' a Product and a User
+# because of the relationship we declared in the migration:
+
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
