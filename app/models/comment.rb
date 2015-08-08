76a5251cfdbb7c1a457d540d80c18cb4929f92bf
Christoph <christoph.schenek@googlemail.com>
2015-08-08 12:25:06 +0200

    changedstyle

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,13 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
+
+# Let’s add some constraints to the comment model. 
+# Because every comment record should be saved with a user ID, 
+# a product ID and a message, it's a good idea to validate the presence 
+# of all of those attributes 
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: {only_integer: true}
 end
