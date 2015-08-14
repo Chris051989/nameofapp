3e857bd6fe20d735936b5e2261fe35a0fee47af7
Christoph <christoph.schenek@googlemail.com>
2015-08-14 14:41:24 +0200

    unit test revised

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,61 +1,78 @@
+# require 'rails_helper' is generally written in the first line 
+
 require 'rails_helper'
+
+
+# Describe specifies the class that I will test. In this case, this is the class products. 
 
 describe Product do
 
 
+# the context block represents different states. 
+# in this case, different states of the product model are presented
+# we thus can create different sets of supporting data that is 
+# used by every test in the context
+# in this test, we do have three different sets of context 
+# 1. context "only name present" 2. context "name, colour, price present" 3.context "Comment present"
+
 context "only name present" do
 
+# with the context section, we do have a so-called before block 
+# we will run the before block before each test in the context 
+# to set up any data we need might need in the test. 
 		before {@product = build(:product)}
 
+# the it block contains the different individual test 
+# in the case below, we expect that the product name will be returned, 
+# and that the product name is expected to equal somethin like Monster
 		it "returns product name" do
-		expect(@product.name).to eq "Amazingwheels" 
+		expect(@product.name).to eq "Monster" 
 		end
 	end
+
+
+
+# here another before section starts which sets up data for the variables
+# product and comment  
 
 	before(:all) do
 		@product = build(:product_full_profile)
 		@comment = build(:comment)
-		# @comment = @product.comments.build(body: "blablub", rating: 4)
-	end
+		
 
+# here the second context starts 
 	context "name, colour, price present" do
+
 		it "returns name" do
-			expect(@product.name).to eq "Amazingwheels"
+			expect(@product.name).to eq "Monster"
 		end
 
 		it "returns colour" do
-			expect(@product.colour).to eq "sparkling-pink"
+			expect(@product.colour).to eq "Yellow"
 		end
 
 		it "returns price" do
-			expect(@product.price).to eq "100"
+			expect(@product.price).to eq "500"
 		end
 
 	end
 
+# third context for the comment section starts here 
 	context "comment present" do
-		
+
+# it returns the written rating, which should equal something like 
+# Nice Product S	
 		it "returns comment body" do
-			expect(@comment.body).to eq "blablub"
+			expect(@comment.body).to eq "Nice Product"
 		end
 
+# we test for the return of a comment rating that equals a numerical value 
 		it "returns comment rating" do
 			expect(@comment.rating).to eq 4
 		end
 
-		xit "returns rating of first comment" do
-			expect(@product.comments.first.rating).to eq 4
-		end
-
-		# Here, I don't know how to make it count all comments.
-		xit "gives correct number of comment for product" do
-			expect(@product.comments.all). to eq 1
-		end
-	end
 	
+	end
+end
 end
 
-
-
-
-# test-driven development - zuerst den Test schreiben, dann den Code
