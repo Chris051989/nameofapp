a0166c73bbe842f503140a49791e7f8435355c72
Christoph <christoph.schenek@googlemail.com>
2015-08-20 13:49:04 +0200

    debugging stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,7 +3,7 @@ def create
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      :amount => params[:productPrice] # charge the amount according to the user ID
+      :amount => params[:productPrice],
       :currency => "usd",
       :source => token,
       :description => "New Order: #{params[:productName]} from #{params[:stripeEmail]}."
@@ -14,6 +14,6 @@ def create
 
     @order = Order.new(:user_id => params[:userID], :product_id => params[:productID], :total => params[:productPrice])
     @order.save
-  end
 end
+
 
