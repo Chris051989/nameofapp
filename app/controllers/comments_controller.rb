36e23bea1ee1408be9ea860d96794eef3ec7e399
Christoph <christoph.schenek@googlemail.com>
2015-09-02 15:05:39 +0200

    debug admin review

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,12 +20,13 @@ class CommentsController < ApplicationController
 		@comment.destroy
 		redirect_to product
 	end
+end
 
-	private
+private
 		def comment_params
 			params.require(:comment).permit(:user_id, :body, :rating)
 		end
-end
+
 
 
 
